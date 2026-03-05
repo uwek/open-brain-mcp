83aabe24b9eb37de09dcba948661d751d32a356c
Uwe Kallmeyer <uwe.kallmeyer@gmail.com>
2026-03-05 23:16:00 +0100

    0.2

---
+++ scripts/cli.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+VENV=.venv-$(uname -m)
+
+cd ~/dev/open-brain-mcp/
+
+if [ ! -d "$VENV" ]; then
+  python3 -m venv $VENV
+fi
+
+. $VENV/bin/activate
+
+# python3 server.py --host 0.0.0.0 --port 4567 --key meingeheimespasswort
+python3 cli.py $* 
